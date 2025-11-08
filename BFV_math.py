b8d5c7f568386fde08f8c4bb662939711876be79
Ali Hammoud <70279295+alibillalhammoud@users.noreply.github.com>
2025-11-08 13:39:24 -0500

    small numbers working

--- BFV_math.py
+++ BFV_math.py
@@ -2,15 +2,15 @@ import numpy as np
 from BFV_config import BFVSchemeConfiguration
 
 
-def naive_polynomial_mult_nomod(config: BFVSchemeConfiguration, a: np.ndarray, b: np.ndarray) -> np.ndarray:
+def naive_polynomial_mult_nomod(config: BFVSchemeConfiguration, a_in: np.ndarray, b_in: np.ndarray) -> np.ndarray:
     """Compute a*b mod x^n+1 naively (without NTT)"""
-    config.validateAB(a,b)
-    n = len(a)
-    c = np.convolve(a, b)
+    config.validate_AB(a_in,b_in)
+    n = len(a_in)
+    cconv = np.convolve(a_in, b_in)
     # compute c mod x^n+1
-    c_mod = c[:n].copy()
-    for k in range(n, len(c)):
-        c_mod[k-n] -= c[k]
+    c_mod = cconv[:n].copy()
+    for k in range(n, len(cconv)):
+        c_mod[k-n] -= cconv[k]
     # if you want mod uncomment this line
     #c_mod %= config.q
     return c_mod
