36808051e8d865cbfe8feeb45f952cb1c387b09b
Ali Hammoud <70279295+alibillalhammoud@users.noreply.github.com>
2025-11-10 18:48:16 -0500

    test mul cipher plain

--- run.py
+++ run.py
@@ -4,11 +4,11 @@ from BFV_model import BFVSchemeClient, BFVSchemeServer
 
 
 if __name__ == "__main__":
-    t = 17             # example prime, pick this to bigger to get bigger results
-    n = 8              # degree + 1 => degree 7 polynomials
-    q = 65299          # q >> t, t divides q, 17*3837
-    message1 = np.array([1 , 2, 3 ,4 ,5 ,6, 7, 8])
-    message2 = np.array([2 , 1, 4 ,3 ,6 ,5, 8, 7])
+    t = 1031    # 1031 is a prime bigger than 2^10 (so we can do 10bit math)
+    n = 128    # we can get 8192 results at one time (polynomial degree = n-1)
+    q = t*3000  # q >> t, t divides q, q must be around 300-330 bits for practical security
+    message1 = np.array([1, 2, 3 ,4 ,5 ,6, 7, 8] + 120*[1])
+    message2 = np.array([2, 3, 4 ,5 ,6 ,7, 8, 9] + 120*[1])
 
     # setup
     config = BFVSchemeConfiguration(t, q, n, False)
@@ -20,7 +20,10 @@ if __name__ == "__main__":
     A1, B1 = cipher1
     A2, B2 = cipher2
     computed_cipher = server.add_ciphercipher(A1, B1, A2, B2)
+    computed_cipherm = server.mul_cipherplain(A1, B1, message2)
     # decrypt
-    Ao, Bo = computed_cipher
+    Ao, Bo = computed_cipherm
     result = client.decrypt(Ao, Bo)
-    print(np.round(result))
+    rounded_result = np.round(result)
+    #print(result)
+    print(rounded_result)
